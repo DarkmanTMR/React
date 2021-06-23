4dff20ae1eb7990fdf0b7dd40c4aef097d6411a9
DarkmanTMR <trp1prt1@gmail.com>
2021-06-23 12:17:04 +0300

    Users done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Menu from './components/menu/Menu';
 export default function App() {
   return (
     <div>
-        <Menu pages={['users page','comments page', 'posts page']}
+        <Menu
+            pages={['users page','comments page', 'posts page']}
         classes ={['aaa', 'bbbb']}
         />
         <Users/>
