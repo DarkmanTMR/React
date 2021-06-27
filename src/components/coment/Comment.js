281948104e2e00b4773e1122f84ff1bd615fa73d
DarkmanTMR <trp1prt1@gmail.com>
2021-06-27 23:25:39 +0300

    added comment.css

--- src/components/coment/Comment.js
+++ src/components/coment/Comment.js
@@ -1,8 +1,10 @@
+import './Comment.css'
 export default function Comment(props) {
+
 
 let {item: comment} = props;
     return(
-    <div>
+    <div className='target'>
         <h3>{comment.id}: {comment.name}  </h3>
         <h4>from: {comment.email}</h4>
         <p>Comment: "{comment.body}"</p>
