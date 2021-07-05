e53eef5fbbd543cb04c9024930f4821ad8082dad
DarkmanTMR <trp1prt1@gmail.com>
2021-07-06 01:37:27 +0300

    User's Post almost done

--- src/services/API.js
+++ src/services/API.js
@@ -4,7 +4,7 @@ let axiosUsers = axios.create({
     baseURL:'https://jsonplaceholder.typicode.com/'
 });
 
-const url ='https://jsonplaceholder.typicode.com/'
+
 const getUsers = () => axiosUsers('users');
 const getPosts = (id) =>  axiosUsers('users/' + id + '/posts');
 
